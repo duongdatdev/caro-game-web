585f64be77cf091689f6d34fd5556e77cc8dce28
duongdat <duongdat030@gmail.com>
2024-11-03 10:56:40 +0700

    fix: can't direct to leaderboard page erro

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import {Link, Head } from '@inertiajs/vue3';
 
 interface PlayerStats {
     wins: number;
