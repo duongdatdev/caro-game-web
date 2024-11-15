612c37bf69fadafc273d88e9b1ea5397211f8d62
duongdat <duongdat030@gmail.com>
2024-11-16 00:15:30 +0700

    feat: ready real-time, change state to playing when all player ready

--- resources/js/Pages/Game/Show.vue
+++ resources/js/Pages/Game/Show.vue
@@ -106,11 +106,15 @@ onMounted(() => {
             isYourTurn.value = e.move.user_id !== props.currentPlayer;
         })
         .listen('.player.ready', (e: any) => {
+            console.log('Ready event received:', e); // Add logging
+            
             // Update player ready status
-            const playerIndex = players.value.findIndex((p: { id: number }) => p.id === e.player_id);
+            const playerIndex = players.value.findIndex(p => p.id === e.playerId); // Update to match event data
             if (playerIndex !== -1) {
-                players.value[playerIndex].pivot.is_ready = e.is_ready;
+                players.value[playerIndex].pivot.is_ready = e.isReady; // Update to match event data
             }
+
+            // Update game status if all players are ready
             if (e.roomStatus === 'playing') {
                 gameStatus.value = 'playing';
                 isYourTurn.value = props.room.created_by === props.currentPlayer;
